3ec5dc10b6a4946cff24a49d3a756e5410e2c708
haesa <xdhaesa@gmail.com>
2024-11-25 22:00:31 +0900

    라우트 연결

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { BrowserRouter, Routes, Route } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.jsx';
+import LikedPage from '../src/pages/LikedPage';
 import './index.css';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/liked' element={<LikedPage />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 );
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,11 +1,16 @@
+import { Link, useNavigate } from 'react-router';
 import SearchBar from '../../components/SearchBar';
 import SongList from '../../components/SongList';
 
 import styles from './SearchPage.module.css';
 
 export default function SearchPage() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
+      <Link to='/liked'>Liked Link</Link>
+      <button onClick={() => navigate('/liked')}>Liked Button</button>
       <SearchBar />
       <SongList />
     </div>
